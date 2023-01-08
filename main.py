04a5ad63ac4d0798618e5497a72f95af9adfe8ee
dvtdat <tiendat0417@gmail.com>
2023-01-08 20:18:55 +0700

    some foundation for the in-built dictionary

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import discord
+import os
+import requests
+import json
+from dotenv import load_dotenv  # pip install python-dotenv, required to load TOKEN from .env
 
-TOKEN = "MTA2MTIzMDA5ODY0MjEwODQyNg.G7Zp-i.SkKiF_fLKiSRNvDfhJg-jsKuKz8qBm4CXk6zj8"
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
 
 client = discord.Client(intents=discord.Intents.all())
+
+def getWord():
+    wordInput = "integrity"
+    wordReturn = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/{}".format(wordInput))
+    jsonData = json.loads(wordReturn.text)
+
+    word = jsonData[0]["word"]
+    wordPhonetic = jsonData[0]["phonetics"]
+    wordMeaning = jsonData[0]["meanings"]
+
+    for result in wordPhonetic:
+        if ("text" in result):
+            return result["text"]
 
 @client.event
 async def on_ready():
@@ -21,5 +40,9 @@ async def on_message(message):
 
     if message.content.startswith("da. god"):
         await message.channel.send("toi qua' gae")
+
+    if message.content.startswith(".dict integrity"):
+        word = getWord()
+        await message.channel.send(word)
 
 client.run(TOKEN)
